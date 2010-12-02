96b4cb2acbdb4654f1ce440cc88be95842c1238c
Tim Alexeevskiy <realtim@gmail.com>
2010-12-02 03:35:44 +0300

    Update summary improvements
    Fields of a post get compared separately and renewed values are
    highlighted in summary.

--- extractors.rb
+++ extractors.rb
@@ -89,6 +89,7 @@ end
 # Какие показывались можно определять по MD5 (хранить архив
 # показанных).
 class LivejournalExtractor
+   DATA_MAX_SIZE=30
    def initialize
       @data = []
       @data.extend(MonitorMixin)
@@ -105,12 +106,39 @@ class LivejournalExtractor
       data.each do |post|
          md5 = Digest::MD5.hexdigest(post.to_s)
          next if @known.include?(md5)
+         unless notify
+            @data.synchronize do
+               @data.each do |p|
+                  p["time_seen"] = true
+                  p["subject_seen"] = true
+                  p["text_seen"] = true
+                  p["comments_seen"] = true
+               end
+            end
+         end
          notify = true
          @known << md5
          i = @data.find_index {|p| p["url"] == post["url"]}
          @data.synchronize do
             if i
-               @data[i] = post
+               # was: data[i] = post
+               @data[i].delete("hide")
+               if @data[i]["time"] != post["time"]
+                  @data[i]["time"] = post["time"]
+                  @data[i].delete("time_seen")
+               end
+               if @data[i]["subject"] != post["subject"]
+                  @data[i]["subject"] = post["subject"]
+                  @data[i].delete("subject_seen")
+               end
+               if @data[i]["text"] != post["text"]
+                  @data[i]["text"] = post["text"]
+                  @data[i].delete("text_seen")
+               end
+               if @data[i]["comments"] != post["comments"]
+                  @data[i]["comments"] = post["comments"]
+                  @data[i].delete("comments_seen")
+               end
             else
                @data << post
             end
@@ -120,14 +148,21 @@ class LivejournalExtractor
    end
    def clear
       @data.synchronize do
-         @data.clear
+         @data.each do |post|
+            post["hide"] = true
+         end
+         # Чтобы не хранить посты вечно
+         # TODO Возможно подчищать надо не только тут
+         @data = @data[@data.size-DATA_MAX_SIZE..@data.size-1] if @data.size > DATA_MAX_SIZE
       end
    end
    def summary
-      @data.map do |post|
-         "#{post["time"]}\n<i>#{post["user"]}</i> <b>#{post["subject"]}</b>\n"\
-         "#{post["text"]}"\
-         "#{post["comments"] ? "\n<span fgcolor=\"#777777\">{#{post["comments"]}}</span>" : ""}"
+      @data.select{|post| !post["hide"]}.map do |post|
+         "#{post["time_seen"] ? "<span fgcolor=\"#777777\">" : ""}#{post["time"]}#{post["time_seen"] ? "</span>" : ""}\n"\
+         "<span fgcolor=\"#007000\"><i>#{post["user"]}</i></span>"\
+         " #{post["subject_seen"] ? "<span fgcolor=\"#777777\">" : ""}<b>#{post["subject"]}</b>#{post["subject_seen"] ? "</span>" : ""}\n"\
+         "#{post["text_seen"] ? "<span fgcolor=\"#777777\">" : ""}#{post["text"]}#{post["text_seen"] ? "</span>" : ""}"\
+         "#{post["comments"] ? (post["comments_seen"] ? "\n<span fgcolor=\"#777777\">{#{post["comments"]}}</span>" : "\n{#{post["comments"]}}") : ""}"
       end.join("\n\n")
    end
 protected
